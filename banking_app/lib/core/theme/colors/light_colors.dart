0bbdfae06c07c039b70e5e518e86894232a77c18
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-09-03 18:13:17 +0100

    create light and dark app colors

---
+++ banking_app/lib/core/theme/colors/light_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+abstract class AppLightColors {
+  static Color primaryColor = const Color(0XFF0066FF);
+  static Color onPrimaryColor = const Color(0XFFFFFFFF);
+  static Color secondaryColor = const Color(0XFF7E848D);
+  static Color onSecondaryColor = const Color(0XFF7E848D);
+  static Color thirdColor = const Color(0XFFF4F4F4);
+  static Color onThirdColor = const Color(0XFF4C4C54);
+  static Color surfaceColor = const Color(0XFFFFFFFF);
+  static Color onSurfaceColor = const Color(0XFF1E1E2D);
+}
