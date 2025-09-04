40ae5d41992639b596971c0577d302f3e69cc95d
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-09-04 18:18:56 +0100

    create shared prefs service

--- banking_app/lib/main.dart
+++ banking_app/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:banking_app/core/router/app_router.dart';
+import 'package:banking_app/core/services/shared_prefs.dart';
 import 'package:banking_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
 
-void main() {
+void main() async {
+  await SharedPrefsSingelton.init();
   runApp(DevicePreview(enabled: true, builder: (context) => BankingApp()));
 }
 
