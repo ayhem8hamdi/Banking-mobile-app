d133583f6ac5862c980ebb9aa7f0bb443b3ffb16
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-09-06 02:02:25 +0100

    create bottom nav tab widget

---
+++ banking_app/lib/core/shared_widgets/bottom_nav_tab.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class NavShell extends StatelessWidget {
+  final StatefulNavigationShell navigationShell;
+
+  const NavShell(this.navigationShell, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ColorScheme cs = Theme.of(context).colorScheme;
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: navigationShell.currentIndex,
+        selectedItemColor: cs.primary,
+        unselectedItemColor: cs.onTertiary,
+        showUnselectedLabels: true,
+        onTap: (index) {
+          navigationShell.goBranch(index);
+        },
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.credit_card),
+            label: "My Cards",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.bar_chart),
+            label: "Statistics",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: "Settings",
+          ),
+        ],
+      ),
+    );
+  }
+}
