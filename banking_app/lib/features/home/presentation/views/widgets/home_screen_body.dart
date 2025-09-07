ab238c48f796fb4c2e769b7087d610fe5f9b6cb4
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-09-07 01:32:26 +0100

    add transaction title

--- banking_app/lib/features/home/presentation/views/widgets/home_screen_body.dart
+++ banking_app/lib/features/home/presentation/views/widgets/home_screen_body.dart
@@ -1,7 +1,8 @@
+import 'package:banking_app/core/utils/styles/app_styles.dart';
 import 'package:banking_app/features/home/presentation/views/widgets/card_widget.dart';
 import 'package:banking_app/features/home/presentation/views/widgets/custom_background_stack.dart';
 import 'package:banking_app/features/home/presentation/views/widgets/home_app_bar.dart';
-import 'package:banking_app/features/home/presentation/views/widgets/home_transactions_row.dart';
+import 'package:banking_app/features/home/presentation/views/widgets/home_transactions_types_row.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -21,8 +22,26 @@ class HomeScreenBody extends StatelessWidget {
             SliverGap(30),
             TransactionsTypeRow(),
             SliverGap(28),
+            TransactionTitle(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class TransactionTitle extends StatelessWidget {
+  const TransactionTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text("Transaction", style: AppStyles.styleMeduim18(context)),
+          Text("See All", style: AppStyles.styleMeduim14(context)),
+        ],
       ),
     );
   }
