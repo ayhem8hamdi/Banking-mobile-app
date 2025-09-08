ee48c82c1b4b7223d0f9c92f25ddaec0664844ba
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-09-09 00:35:36 +0100

    create settings titles widget

--- banking_app/lib/features/settings/presentation/widget/settings_screen_body.dart
+++ banking_app/lib/features/settings/presentation/widget/settings_screen_body.dart
@@ -1,6 +1,5 @@
-import 'package:banking_app/features/home/presentation/views/widgets/custom_background_stack.dart';
+import 'package:banking_app/core/utils/styles/app_styles.dart';
 import 'package:banking_app/features/stats/presentation/views/widgets/stats_custom_app_bar.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 
@@ -19,7 +18,22 @@ class SettingsScreenBody extends StatelessWidget {
             rightIcon: Icons.logout_outlined,
           ),
           SliverGap(32),
+          SettingsTitles(text: "General"),
         ],
+      ),
+    );
+  }
+}
+
+class SettingsTitles extends StatelessWidget {
+  const SettingsTitles({super.key, required this.text});
+  final String text;
+  @override
+  Widget build(BuildContext context) {
+    return SliverToBoxAdapter(
+      child: Padding(
+        padding: const EdgeInsets.only(left: 4),
+        child: Text(text, style: AppStyles.styleRegular14(context)),
       ),
     );
   }
