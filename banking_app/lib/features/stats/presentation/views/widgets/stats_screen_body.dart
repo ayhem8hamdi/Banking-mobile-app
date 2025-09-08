a0925372582c62e97a06dca20a8cc2289e611b38
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-09-08 17:17:35 +0100

    create stats custom app bar

--- banking_app/lib/features/stats/presentation/views/widgets/stats_screen_body.dart
+++ banking_app/lib/features/stats/presentation/views/widgets/stats_screen_body.dart
@@ -1,5 +1,11 @@
+import 'package:banking_app/core/utils/styles/app_styles.dart';
+import 'package:banking_app/features/auth/presentation/views/widgets/auth_custom_app_bar.dart';
 import 'package:banking_app/features/home/presentation/views/widgets/custom_background_stack.dart';
+import 'package:banking_app/features/home/presentation/views/widgets/home_app_bar.dart';
+import 'package:banking_app/features/stats/presentation/views/widgets/stats_custom_app_bar.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 
 class StatsScreenBody extends StatelessWidget {
   const StatsScreenBody({super.key});
@@ -9,8 +15,14 @@ class StatsScreenBody extends StatelessWidget {
     return const CustomBackgroundStack(
       child: Padding(
         padding: EdgeInsets.symmetric(horizontal: 22),
-        child: CustomScrollView(slivers: [
-
+        child: CustomScrollView(
+          slivers: [
+            StatsCustomAppBar(
+              text: "My Cards",
+              leftIcon: Icons.arrow_back_ios_new,
+              rightIcon: CupertinoIcons.add,
+            ),
+            SliverGap(32),
           ],
         ),
       ),
