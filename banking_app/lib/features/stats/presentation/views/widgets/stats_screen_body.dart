f0d3564f76a83db6eb360085ce79ec44dfb98f5d
ayhem8hamdi <ayhemhamdi24@gmail.com>
2025-09-08 19:09:43 +0100

    create range slider widget

--- banking_app/lib/features/stats/presentation/views/widgets/stats_screen_body.dart
+++ banking_app/lib/features/stats/presentation/views/widgets/stats_screen_body.dart
@@ -1,3 +1,4 @@
+import 'package:banking_app/core/utils/styles/app_styles.dart';
 import 'package:banking_app/features/home/presentation/views/widgets/card_widget.dart';
 import 'package:banking_app/features/home/presentation/views/widgets/custom_background_stack.dart';
 import 'package:banking_app/features/home/presentation/views/widgets/transaction_sliver_list.dart';
@@ -35,9 +36,122 @@ class StatsScreenBody extends StatelessWidget {
             SliverGap(7),
             MonthlySpendingTitle(),
             SliverGap(19),
+            MonthlySpendingRangeContainer(),
           ],
         ),
       ),
+    );
+  }
+}
+
+class MonthlySpendingRangeContainer extends StatelessWidget {
+  const MonthlySpendingRangeContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final cs = Theme.of(context).colorScheme;
+    return SliverToBoxAdapter(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 23),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: cs.tertiary,
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              r"Amount: $8,545.00",
+              style: AppStyles.styleRegular13(
+                context,
+              ).copyWith(color: cs.onSurface),
+            ),
+            const Gap(11),
+            const SingleValueSlider(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SingleValueSlider extends StatefulWidget {
+  const SingleValueSlider({super.key});
+
+  @override
+  State<SingleValueSlider> createState() => _SingleValueSliderState();
+}
+
+class _SingleValueSliderState extends State<SingleValueSlider> {
+  double _currentValue = 5000;
+
+  @override
+  Widget build(BuildContext context) {
+    final cs = Theme.of(context).colorScheme;
+    final theme = Theme.of(context);
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        double sliderWidth = constraints.maxWidth;
+        double thumbPercent = _currentValue / 10000;
+        double thumbX = thumbPercent * (sliderWidth - 26);
+
+        return Stack(
+          clipBehavior: Clip.none,
+          alignment: Alignment.centerLeft,
+          children: [
+            SliderTheme(
+              data: SliderTheme.of(context).copyWith(
+                trackHeight: 4,
+                activeTrackColor: theme.brightness == Brightness.dark
+                    ? Colors.white
+                    : cs.primary,
+                inactiveTrackColor: cs.surface,
+                thumbColor: Colors.white,
+                overlayColor: cs.primary,
+                thumbShape: const RoundSliderThumbShape(
+                  enabledThumbRadius: 8,
+                  disabledThumbRadius: 8,
+                ),
+                overlayShape: const RoundSliderOverlayShape(overlayRadius: 11),
+              ),
+              child: Slider(
+                value: _currentValue,
+                min: 0,
+                max: 10000,
+                divisions: 10000,
+                onChanged: (value) {
+                  setState(() {
+                    _currentValue = value;
+                  });
+                },
+              ),
+            ),
+
+            Positioned(
+              left: thumbX,
+              bottom: -14,
+              child: Text(
+                "\$${_currentValue.round().toString()}",
+                style: AppStyles.styleRegular12(
+                  context,
+                ).copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+            ),
+
+            Positioned(
+              left: 9,
+              bottom: 20,
+              child: Text(r"$0", style: AppStyles.styleRegular12(context)),
+            ),
+
+            Positioned(
+              right: 10,
+              bottom: 20,
+              child: Text(r"$10,000", style: AppStyles.styleRegular12(context)),
+            ),
+          ],
+        );
+      },
     );
   }
 }
